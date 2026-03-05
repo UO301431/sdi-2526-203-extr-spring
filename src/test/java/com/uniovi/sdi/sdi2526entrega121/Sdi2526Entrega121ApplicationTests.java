b47116d5bb68e8b6926f266222f575b1df92c87a
Alejandro Fernández García <alejandro3620@hotmail.es>
2026-03-05 13:13:29 +0100

    Initial commit

---
+++ src/test/java/com/uniovi/sdi/sdi2526entrega121/Sdi2526Entrega121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2526entrega121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2526Entrega121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
