dc83b99c1fd7bf3d0f7669808144f013885d3685
uo288339 <uo288339@uniovi.es>
2026-03-11 15:55:09 +0100

    Se crea el find de espacios activos

--- src/main/java/com/uniovi/sdi/sdi2526entrega121/repositories/SpaceRepository.java
+++ src/main/java/com/uniovi/sdi/sdi2526entrega121/repositories/SpaceRepository.java
@@ -3,10 +3,14 @@ package com.uniovi.sdi.sdi2526entrega121.repositories;
 import com.uniovi.sdi.sdi2526entrega121.entities.Space;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpaceRepository extends CrudRepository<Space, Long> {
 
     Page<Space> findAll(Pageable pageable);
+
+    @Query("SELECT sp from Space sp where sp.active = true")
+    Page<Space> findActiveSpaces(Pageable pageable);
 
 }
