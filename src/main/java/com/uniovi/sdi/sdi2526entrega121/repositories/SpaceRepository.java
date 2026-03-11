5d608aa6a7d39e41ae8b7e28ddcc4d35190e7cae
uo288339 <uo288339@uniovi.es>
2026-03-11 16:47:04 +0100

    Correccion

--- src/main/java/com/uniovi/sdi/sdi2526entrega121/repositories/SpaceRepository.java
+++ src/main/java/com/uniovi/sdi/sdi2526entrega121/repositories/SpaceRepository.java
@@ -20,7 +20,7 @@ public interface SpaceRepository extends CrudRepository<Space, Long> {
     Page<Space> findActiveSpacesByCapacity(Pageable pageable, Integer searchCapacity);
 
     @Query("SELECT sp from Space sp where sp.active = true " +
-            "and (lower(sp.type) like lower(?1) " +
+            "and (lower(sp.type) like lower(?1)) " +
             "and (sp.capacity >= ?2)")
     Page<Space> findActiveSpacesByTypeAndCapacity(Pageable pageable, String searchType, Integer searchCapacity);
 }
