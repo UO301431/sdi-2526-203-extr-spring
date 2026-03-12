3430faa3912eca19f51787c12a3853922beb4b0e
uo294656 <uo294656@uniovi.es>
2026-03-12 13:17:09 +0100

    Documentacion del UsersController

--- src/main/java/com/uniovi/sdi/sdi2526entrega121/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi/sdi2526entrega121/controllers/UsersController.java
@@ -17,6 +17,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
+/**
+ * Controlador encargado de la gestion de usuarios y procesos de la autenticacion
+ * Maneja el registro de nuevos usuarios, el acceso al sistema y la redireccion a la vista principal
+ * @author Pablo Roces
+ * @version 1.0
+ *
+ */
 @Controller
 public class UsersController {
 
@@ -32,6 +39,15 @@ public class UsersController {
         this.signUpFormValidator = signUpFormValidator;
     }
 
+    /**
+     * Procesa el formulario de registro de un nuevo usuario
+     * Realiza validaciones personalizadas y si tiene exito, registra al nuevo usuario
+     * en el sistema e inicia su sesion automaticamente
+     * @param user El objeto {@link User} vinculado a los campos del formulario
+     * @param result Objeto que contiene los posibles errores de validacion
+     * @param model El modelo de la vista
+     * @return El nombre de la vista de registro si hay errores o redireccion a home si es correcto
+     */
     @PostMapping("/signup")
     public String signup(@ModelAttribute User user, BindingResult result, Model model) {
         signUpFormValidator.validate(user,result);
@@ -45,18 +61,36 @@ public class UsersController {
         return "redirect:home";
     }
 
+    /**
+     *  Muestra el formulario de registro al usuario
+     *  Inicializa un objeto {@link User} vacio para el binding del formulario
+     * @param model El modelo de la vista para añadir el objeto usuario
+     * @return El nombre de la vista "signup"
+     */
     @GetMapping("/signup")
     public String signup(Model model) {
         model.addAttribute("user", new User());
         return "signup";
     }
 
+    /**
+     * Gestiona la peticion a la pagina de inicio del sistema
+     * Recupera la informacion de autenticacion del contexto de seguridad
+     * @param model El modelo de la vista
+     * @param pageable Informacion de paginacion opcional
+     * @param principal El usuario autenticado actualmente
+     * @return El nombre de la vista "/home"
+     */
     @RequestMapping("/home")
     public String home(Model model, Pageable pageable, Principal principal) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return "/home";
     }
 
+    /**
+     * Muestra la vista de inicio de sesion
+     * @return El nombre de la vista "login"
+     */
     @GetMapping("/login")
     public String login(){
         return "login";
